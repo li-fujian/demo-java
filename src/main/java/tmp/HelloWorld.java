d9f7a2d36ab3dc66971f49e8ffa6da0823853d1d
lfujian <lifujian@wilddog.com>
2016-12-08 21:39:08 +0800

    commit

---
+++ src/main/java/tmp/HelloWorld.java
@@ -0,0 +1,12 @@
+package tmp;
+
+/**
+ * @author lfj  2016年12月8日
+ * @Description: Good!
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("hello,world~");
+    }
+}
